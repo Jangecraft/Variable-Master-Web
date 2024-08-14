bc3638a4be610a810eaf21eb465e56f5e5841a0b
Jangecraft <78905953+Jangecraft@users.noreply.github.com>
2024-08-14 14:42:18 +0700

    เพิ่ม prompt ใน string

--- promptedString.js
+++ promptedString.js
@@ -3,6 +3,8 @@ import { getRandomInt, getRandomFloat} from "./random.js";
 export function generatePromptedString() {
     const prompts = [
         "หมายเลขบัตรประชาชน",
+        "เบอร์โทรศัพท์",
+        '"เก้าอี้เป็นสีดำ"',
         `บ้านเลขที่ ${getRandomInt(1, 99)} / ${getRandomInt(1, 99)}`
     ];
     const selectedPrompt = prompts[Math.floor(Math.random() * prompts.length)];
